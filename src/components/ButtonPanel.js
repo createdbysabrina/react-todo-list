a754b0559ec86c2494fb4c9c105d2d6f88d4fece
Sabrina Lamont <sabrina.lamont@tmsw.com>
2022-12-03 10:16:15 +0000

    add button panel

---
+++ src/components/ButtonPanel.js
@@ -0,0 +1,16 @@
+export default function ButtonPanel() {
+
+    return (
+        <div className="button-panel">
+            <button className="completed-btn"> 
+                Completed
+            </button>
+            <button className="deleted-btn"> 
+                Delete
+            </button>
+            <button className="archive-btn"> 
+                Archive
+            </button> 
+        </div>
+    )
+}
